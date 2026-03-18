44296685a6416ee19d5ff3e575d272b96c139d5a
Gunveer Kalsi <gunveerkalsi@gmail.com>
2026-03-18 15:03:38 +0530

    fix test

--- test_app.py
+++ test_app.py
@@ -1,11 +1,10 @@
 from app import add, multiply, divide
 
 def test_add():
-    assert add(2,3) == 10   # ❌ intentionally wrong
+    assert add(2,3) == 5   # ✅ fixed
 
 def test_multiply():
     assert multiply(3,4) == 12
 
 def test_divide():
     assert divide(10,2) == 5
-
